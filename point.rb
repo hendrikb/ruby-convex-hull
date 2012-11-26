91a66ca61ed3a9e2a88cac68d5373a61be157f46
Hendrik Bergunde <hbergunde@gmx.de>
2012-11-26 22:19:42 +0100

    implement left_of method

--- point.rb
+++ point.rb
@@ -5,6 +5,18 @@ class Point
     self.y=y
   end
 
+  def is_left_of? x1, x2
+
+  end
+
+  def left_of? p,q
+    r = self
+    determinant = p.x * q.y + r.x*p.y + q.x*r.y - q.x*p.y - r.x*q.y - p.x*r.y
+
+    return true if determinant > 0
+    return false
+  end
+
   def to_s
     "(#{x}:#{y})"
   end
