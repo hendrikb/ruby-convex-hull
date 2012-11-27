d91a8f882e36c8e00a430d0fb2572746dfcf80c5
Hendrik Bergunde <hbergunde@gmx.de>
2012-11-27 15:48:13 +0100

    white space

--- lib/edge.rb
+++ lib/edge.rb
@@ -1,12 +1,12 @@
 module Geometry
-class Edge
-  attr_accessor :p1,:p2
-  def initialize p1,p2
-    self.p1 = p1
-    self.p2 = p2
+  class Edge
+    attr_accessor :p1,:p2
+    def initialize p1,p2
+      self.p1 = p1
+      self.p2 = p2
+    end
+    def to_s
+      "#{p1}-#{p2}"
+    end
   end
-  def to_s
-    "#{p1}-#{p2}"
-  end
-end
 end
