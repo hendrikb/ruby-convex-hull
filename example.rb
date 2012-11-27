0969edd6601a0f2e085d8536d87be52fa0d015c0
Hendrik Bergunde <hbergunde@gmx.de>
2012-11-27 15:59:53 +0100

    run example #2 only

--- example.rb
+++ example.rb
@@ -30,7 +30,7 @@ end
 
 ## USAGE:
 # Example 1: (note how p(3,5) is not part of the hull anymore)
-puts Geometry::ConvexHull.compute [ p(1,1), p(2,1), p(3,5), p(4,3), p(7,3)], true
+# puts Geometry::ConvexHull.compute [ p(1,1), p(2,1), p(3,5), p(4,3), p(7,3)], true
 
 # Example 2
 puts Geometry::ConvexHull.compute load_example_points, true
